fc33505825931554a291e8f091caeb2c761e3296
shireenZakeri <110161167+shireenz1995zakeri@users.noreply.github.com>
2023-09-16 12:00:56 +0330

    test for git 2

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-
+////////////////////////////////////////
 class BaseRepository implements BaseReposirotyInterface
 {
 
